40a786153e0a1f2ef2a6e139214ee5107b9ffd1c
half-shell <michel.maubert.pro@gmail.com>
2016-06-01 16:44:19 +0200

    Dev: Add new test

--- test/index.specs.js
+++ test/index.specs.js
@@ -1,11 +1,45 @@
 var request = require('supertest');
 var api = require('../index');
 
-describe('api', function(){
-    it('should return Hello worl on GET /', function(){
-        return request(api)
-            .get('/')
+describe('contacts', function(){
+    describe('GET /contacts', function(){
+        it('should return the list of contacts', function(){
+            return request(api)
+                .get('/contacts')
+                .send()
+                .expect(200)
+        });
+    });
+    describe('POST /contacts/:name', function(){
+        it('should return a new contact', function(){
+            return request(api)
+                .post('/contacts/foo')
+                .send()
+                .expect(200)
+        });
+        it('should no be authorized to create a new contact with an already registered name', function(){
+            return request(api)
+                .post('/contacts/exists')
+                .send()
+                .expect(401)
+        });
+    });
+    describe('PUT /contacts/:name/:new', function(){
+        it('should update contact with the same name', function(){
+            return request(api)
+            .put('/contacts/foo/bar')
             .send()
-            .expect('Hello foobar');
-    })
+            .expect(200)
+        });
+    });
+    describe('DELETE /contacts/:name', function(){
+        it('should remove contacts with the same name', function(){
+            return request(api)
+                .delete('/contacts/foo')
+                .send()
+                .expect(200)
+        });
+    });
 });
+
+
