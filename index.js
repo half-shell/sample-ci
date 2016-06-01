e7e67883aeb3ded4a482486c1aa1a5a9ed07b122
half-shell <michel.maubert.pro@gmail.com>
2016-06-01 15:47:07 +0200

    Ft: Add basic routing

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var api = express();
+
+api.get('/', function(req, res, next){
+    res.send("Hello world");
+});
+
+var port = '3000';
+console.log('Server started on port ' + port);
+api.listen(port);
